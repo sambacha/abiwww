f5572c6f903b77f0872ccc8598b8fc077ead81c4
sam bacha <sam@freighttrust.com>
2021-03-28 15:19:17 -0700

    chore(app): better logging notifcations

--- src/api/routers/health-check.router.ts
+++ src/api/routers/health-check.router.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 export const healthCheckRouter = express.Router();
 
 healthCheckRouter.get('/healthcheck', (_req, res) => {
-  console.log('healthcheck triggered');
+  console.log('🟠 Healthcheck Triggered');
   res.status(200).send({
     status: 'ok',
   });
